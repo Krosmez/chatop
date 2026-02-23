c8a8ef9cfb912bf40118011c192c08085b4655ec
Krosmez <jonathanmaison78@gmail.com>
2026-02-23 11:16:49 +0100

    fix(image): remove dead code

--- src/main/java/com/chatop/utils/ImageUtils.java
+++ src/main/java/com/chatop/utils/ImageUtils.java
@@ -11,36 +11,19 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 public class ImageUtils {
-  //  public static String handleImageUpload(MultipartFile picture) throws IOException {
-//    if (picture == null || picture.isEmpty()) {
-//      throw new IllegalArgumentException("Aucune image fournie.");
-//    }
-//    String originalFilename = picture.getOriginalFilename();
-//    if (originalFilename == null ||
-//            !(originalFilename.toLowerCase().endsWith(".jpg") ||
-//                    originalFilename.toLowerCase().endsWith(".jpeg") ||
-//                    originalFilename.toLowerCase().endsWith(".png"))) {
-//      throw new IllegalArgumentException("Seuls les fichiers JPG, JPEG et PNG sont acceptés.");
-//    }
-//    String uploadsDir = "uploads/";
-//    Files.createDirectories(Paths.get(uploadsDir));
-//    String filename = System.currentTimeMillis() + "_" + originalFilename;
-//    Path filePath = Paths.get(uploadsDir, filename);
-//    Files.copy(picture.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
-//    return "/api/images/" + filename;
-//  }
+
   public static String handleImageUpload(MultipartFile picture) throws IOException {
-    if (picture == null || picture.isEmpty()) {
+    if(picture == null || picture.isEmpty()) {
       return null;
     }
     String uploadDir = "uploads/";
     File directory = new File(uploadDir);
-    if (!directory.exists()) {
+    if(!directory.exists()) {
       directory.mkdirs();
     }
     String filename = System.currentTimeMillis() + "_" + picture.getOriginalFilename();
     Path filePath = Paths.get(uploadDir + filename);
-    try (InputStream inputStream = picture.getInputStream()) {
+    try(InputStream inputStream = picture.getInputStream()) {
       Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
     }
     return "http://localhost:8080/api/images/" + filename; // Retourner l'URL complète
